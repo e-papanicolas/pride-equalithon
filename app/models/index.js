51c78b1fcd5b6654b710f44ad66ad5b7bd3cba46
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-18 12:25:55 -0400

    updated database connection details

---
+++ app/models/index.js
@@ -0,0 +1,27 @@
+import dbConfig from "../config/db.config.js";
+import { Sequelize } from "sequelize";
+// import users from "./user.model.js";
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+// db.users = users(sequelize, Sequelize);
+
+try {
+  await sequelize.authenticate();
+  console.log("sequelize connection successful");
+} catch (err) {
+  console.error("unable to connect to database");
+}
+
+export { db };
