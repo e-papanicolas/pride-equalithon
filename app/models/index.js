bc1f5a270e8d2f8b7cde0a2ead95ac920d172066
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-18 17:28:53 -0400

    removed old db.config file and put temp info in here

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,35 @@
-import dbConfig from "../config/db.config.js";
 import { Sequelize } from "sequelize";
 import users from "./user.model.js";
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const dbConfig = {
+  HOST: "localhost",
+  USER: process.env.DB_USER,
+  PASSWORD: null,
+  DB: process.env.DB,
+  dialect: "postgres",
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
-});
+};
+
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  }
+);
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
