c237af31383c3396e5a7cba98180d2266bd6ae71
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-20 13:50:07 -0400

    first user route test complete

--- test/user.test.js
+++ test/user.test.js
@@ -1,4 +1,4 @@
-import chai from "chai";
+import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import app from "../server.js";
 
@@ -7,12 +7,17 @@ chai.use(chaiHttp);
 
 describe("User Unit Tests", () => {
   describe("Test GET route /user", () => {
-    it("should return a list of all users ", async () => {
-      (await chai.request(app).get("/user/")).send().end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("array");
-        done();
-      });
+    it("should return a 200 status code", async () => {
+      chai
+        .request(app)
+        .get("/user")
+        .send()
+        .then((res) => {
+          expect(res).to.have.status(200);
+        })
+        .catch((err) => {
+          throw err;
+        });
     });
     it("should throw an error if the user is not added to the database", async () => {});
   });
