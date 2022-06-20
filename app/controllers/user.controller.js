8f7455da9423fe0fb396571b3540273891423859
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-20 18:31:30 -0400

    added actions for refresh tokens, added refresh tokens to login and register

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,3 +1,12 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import {
+  generateRefreshToken,
+  generateAccessToken,
+  addTokenToList,
+  refreshList,
+} from "../helpers/tokens.js";
 import { db } from "../database/models/index.js";
 
 const User = db.users;
@@ -10,16 +19,75 @@ userActions.new = async (req, res, next) => {
   }
   // create instance and save to db
   try {
-    console.log(req.body);
-    let newUser = await User.create({
+    let newUser = await User.findOne({
+      where: { username: req.body.username },
+    });
+    if (newUser) return res.status(400).json({ message: "username is taken" });
+    // password encryption
+    const salt = await bcrypt.genSalt(10);
+    const pswd = await bcrypt.hash(req.body.password, salt);
+    // create the instance with the hashed password
+    newUser = User.build({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: pswd,
     });
-    console.log(newUser);
-    res.status(200).json(newUser);
+    await newUser.save();
+
+    // payload for signing the JWT token
+    const payload = {
+      user: {
+        id: newUser.id,
+      },
+    };
+
+    // generates the tokens and adds to the list for refresh
+    const token = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+    addTokenToList(refreshToken, token);
+
+    res.status(200).json({ token, refreshToken, newUser });
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+};
+
+userActions.login = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({
+      where: { username: username },
+    });
+    if (!user) return res.status(404).json({ message: "invalid username" });
+
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) return res.status(404).json({ message: "invalid password" });
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    const token = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+    addTokenToList(refreshToken, token);
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+};
+
+userActions.tokenRefresh = async (req, res, next) => {
+  const data = req.body;
+  if (data.refreshToken && data.refreshToken in refreshList) {
+    const decoded = jwt.verify(data.refreshToken, process.env.JWT_R_KEY);
+    const token = generateAccessToken(decoded.id);
+    const refreshToken = generateRefreshToken(decoded.id);
+    addTokenToList(refreshToken, token);
+    res.status(200).json({ user: decoded, token, refreshToken });
+  } else {
+    res.status(401).json({ message: "Invalid refresh token" });
   }
 };
 
